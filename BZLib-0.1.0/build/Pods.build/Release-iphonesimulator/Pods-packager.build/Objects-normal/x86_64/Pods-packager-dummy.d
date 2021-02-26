c4e5088fffa63254323a65b0d778bd08a6a0d305
ye hao <yehao@codoon.com>
2021-02-26 17:05:23 +0800

    0.5.0

--- BZLib-0.1.0/build/Pods.build/Release-iphonesimulator/Pods-packager.build/Objects-normal/x86_64/Pods-packager-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /var/folders/hm/3ggf159s5zs33m_c_6kz0kgr0000gn/T/cocoapods-1l8a6rs8/Pods/Target\ Support\ Files/Pods-packager/Pods-packager-dummy.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.0.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.0.sdk/usr/include/mach-o/dyld.modulemap
